52e90a38415a973ce503ea1b20fa2ff1da6523f9
merveeustalar <merveustalarr@gmail.com>
2025-10-17 20:42:06 +0300

    Çarpma işlemi eklendi

--- server.js
+++ server.js
@@ -15,6 +15,12 @@ app.post("/sum", (req, res) => {
     const { num1, num2 } = req.body;
     res.json({ result: num1 + num2 });
 });
+app.post("/multiply", (req, res) => {
+    const { number1, number2 } = req.body;
+    const result = Number(number1) * Number(number2);
+    res.json({ result });
+  });
+  
 
 app.listen(PORT, () => {
     console.log(`Sunucu ${PORT} portunda çalışıyor.`);
